d1b58855d8204a417fca2060cf9e345db40d7168
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-07-06 19:07:42 -0700

    everything working

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 // Your code goes here
-const nav=document.querySelector('.nav')
+const nav=document.querySelectorAll('.nav a')
 const logo=document.querySelector('.logo-heading')
 const welcome=document.querySelector('.intro h2')
 const introText=document.querySelector('.intro p')
 const heading=document.querySelector('.content-section h2')
 const head=document.getElementsByTagName('h4')
-// const lets=document.querySelector('h2,h4')
-// const letse=document.querySelector('.content-section .text-content')
+const heads=document.getElementsByTagName('h2')
+const text=document.getElementsByTagName('p')
+// const lets=document.querySelector('')
+
 // const letsImg=document.querySelector('.content-section .img-content')
 const load=function(event){
     document.body.style.color="purple"
@@ -16,8 +18,18 @@ window.addEventListener('load', load)
 logo.addEventListener('dblclick', function(event){
     logo.style.color='blue'
 })
-nav.addEventListener('mouseover', function(event){
-    nav.style.backgroundColor='red'
+// nav.addEventListener('mouseover', function(event){
+//     nav.style.backgroundColor='red'
+//     nav.style.color='blue'
+//        // elem.style.backgroundColor='red'
+//        window.prompt("Great Choice!!!!!  \n\nEnter your Email")
+// })
+nav.forEach(elem => {
+    elem.addEventListener('click', function (event) {
+        elem.style.color='blue'
+        elem.style.backgroundColor='pink'
+       window.prompt("Great Choice!!!!!  \n\nEnter your Email")
+    })
 })
 welcome.addEventListener('wheel', function(event){
     welcome.style.color='green'
@@ -29,14 +41,28 @@ introText.addEventListener('mouseover', function(event){
     heading.onclick=function(event){
         heading.style.color='green'
     }
-    head.onclick=function(event){
-        head.style.color='teal'
-    }
-    // head.addEventListener('mouseover', function(event){
-    //     head.style.color='teal'
-
-    // })
-// })
+    const letse=document.querySelectorAll('.content-section .text-content p')
+    Array.from(letse).forEach(elem => {
+        elem.addEventListener('click', function (event) {
+            elem.style.color='teal'
+        })
+    })
+    Array.from(head).forEach(elem => {
+        elem.addEventListener('click', function (event) {
+            elem.style.color='green'
+        })
+    })
+    Array.from(heads).forEach(elem => {
+        elem.addEventListener('click', function (event) {
+            elem.style.color='green'
+        })
+    })
+    Array.from(text).forEach(elem => {
+        elem.addEventListener('click', function (event) {
+            elem.style.color='teal'
+        })
+    })
+// // })
 // heading.addEventListener('click', function(event){
 //     // heading.style.color='green'
 // })
